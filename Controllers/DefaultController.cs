63022d0a54571c7324a315b9c47789c1ecf73083
George Luan <ofgreg@gmail.com>
2016-12-07 18:46:20 -0200

    Primeiro Projeto

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+[RoutePrefix("api/meuprojeto")]
+public class DefaultController : ApiController
+{
+    [HttpGet]
+    [Route("datahora/consulta")]
+    public HttpResponseMessage GetDataHoraServidor()
+    {
+        try
+        {
+            var dataHora = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+
+            return Request.CreateResponse(HttpStatusCode.OK, dataHora);
+        }
+        catch (Exception ex)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+        }
+    }
+}
